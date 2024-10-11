aabb2657fef723f34124f297a39bb0000a1864b0
PallaviPYXEDA <149144280+PallaviPYXEDA@users.noreply.github.com>
2024-10-12 05:14:42 +0530

    Update 1_Project_Information.py

--- pages/1_Project_Information.py
+++ pages/1_Project_Information.py
@@ -3,8 +3,7 @@ import streamlit as st
 st.title('Know your ICs better')
 
 st.header('About:')
-st.markdown('Integrated circuit (IC) chips are vital for devices like phones and computers, acting as their \"brains.\" To boost interest in the growing U.S. IC industry, I created the AI-powered app \"Know the ICs Better.\" This interactive chatbot helps users learn about ICs, identifying types, providing details, detecting defects, and making learning engaging with a Q&A section.
-')
+st.markdown("Integrated circuit (IC) chips are vital for devices like phones and computers, acting as their 'brains.' To boost interest in the growing U.S. IC industry, I created the AI-powered app 'Know the ICs Better' This interactive chatbot helps users learn about ICs, identifying types, providing details, detecting defects, and making learning engaging with a Q&A section.")
 
 
 img1 = "img1.png"
