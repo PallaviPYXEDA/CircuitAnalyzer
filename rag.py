40b414a55cef2c1318826881a3fef254e9486294
PallaviPYXEDA <149144280+PallaviPYXEDA@users.noreply.github.com>
2024-10-18 18:47:31 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -143,7 +143,7 @@ def call_vision_api(image_path:str):
     "Authorization": f"Bearer {os.environ["OPENAI_API_KEY"]}"
     }
 
-    prompt = "You are an expert assisstant trained to identify different electronic chip types. You are given an image of a chip. First carefully go over the images and classify the image into one out of four categories which are mentioned next. Analog ICs (Op-Amps, Voltage Regulators, ADCs),ASIC and SOC, Digital ICs (Microprocessors, Memory Chips, DSPs) and Mixed-Signal ICs (Modems, Codecs, Mixed-Signal Processors). Please respond with only the classified name. If you can't classify the image into given 4 classes, please say you don't have the context."
+    prompt = "You are an expert assisstant trained to identify different electronic chip types. You are given an image of a chip. First carefully go over the images and classify the image into one out of four categories which are mentioned next. Analog ICs (Op-Amps, Voltage Regulators, ADCs),ASIC and SOC, Digital ICs (Microprocessors, Memory Chips, DSPs) and Mixed-Signal ICs (Modems, Codecs, Mixed-Signal Processors). Please respond with only the classified name. If you can't classify the image into given 4 classes, please say you don't have the context. Use the text on the chip, physical characteristics, and any available labeling to determine the most appropriate category."
 
     payload = {
     "model": QA_MODEL,
